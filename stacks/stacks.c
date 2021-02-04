792b3361cd1e92e5cec57051f7c07fce43e53034
Sahithi <sahithi.tanneeru@gmail.com>
2021-02-04 22:47:57 +0530

    updated

---
+++ stacks/stacks.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+struct stack
+{
+    int a[5];
+    int top;
+    int max;
+};
+main()
+{
+    struct stack s;
+    s.top=-1;
+    s.max=5;
+    int opt,ele;
+    do
+    {
+        printf("1.Push\n2.Pop\n3.Exit\n");
+        printf("enter your choice:");
+        scanf("%d",&opt);
+        switch(opt){
+            case 1:
+            {
+                    if(s.top==s.max-1)
+                        printf("stack is full\n");
+                    else
+                    {
+                        s.top++;
+                        scanf("%d",&ele);
+                        s.a[s.top]=ele;
+                    }
+                    break;
+            }
+            case 2:
+            {
+                if(s.top==-1)
+                    printf("stack is empty\n");
+                else
+                {
+                    printf("%d",s.a[s.top]);
+                    s.top--;
+                }
+                break;
+            }
+            case 3:
+            {
+                printf("process terminated");
+                break;
+            }
+            default:{
+                printf("enter valid inputs");
+                break;}
+        }
+
+    }
+    while(opt!=3)
+    ;
+}
+
